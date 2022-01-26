35d4ed03e80d666db91fa5f7e7babddf5b1d60bb
Aditya <47073054+Adityaphalswal@users.noreply.github.com>
2022-01-26 15:04:17 +0530

    Add beep function when dustbin is full

--- main.cpp
+++ main.cpp
@@ -1,53 +1,83 @@
 #include <Servo.h>
 #define LED 13
+#define LED2 12
 #define echoPin 2 
 #define trigPin 3 
+#define echoPin2 4 
+#define trigPin2 5 
+#define buzzer 7 
 
-// defines variables
+
 Servo servoMotor;
-long duration; // variable for the duration of sound wave travel
-int distance; // variable for the distance measurement
-
+long duration;
+long duration2;
+int distance; 
+int distance2;
 
 void setup() {
   servoMotor.attach(5);
-  pinMode(trigPin, OUTPUT); // Sets the trigPin as an OUTPUT
-  pinMode(echoPin, INPUT); // Sets the echoPin as an INPUT
-  Serial.begin(9600); // // Serial Communication is starting with 9600 of baudrate speed
-  Serial.println("Ultrasonic Sensor HC-SR04 Test"); // print some text in Serial Monitor
+  pinMode(trigPin, OUTPUT); 
+  pinMode(echoPin, INPUT); 
+  pinMode(trigPin2, OUTPUT); 
+  pinMode(echoPin2, INPUT);
+  Serial.begin(9600);
+  Serial.println("Ultrasonic Sensor HC-SR04 Test"); 
   Serial.println("with Arduino UNO R3");
   pinMode(LED, OUTPUT);
+  pinMode(LED2, OUTPUT);
+  pinMode(buzzer, OUTPUT);
 }
 void loop() {
   servoMotor.write(0);
-  // Clears the trigPin condition
   digitalWrite(trigPin, LOW);
+  digitalWrite(trigPin2, LOW);
   delayMicroseconds(2);
-  // Sets the trigPin HIGH (ACTIVE) for 10 microseconds
   digitalWrite(trigPin, HIGH);
+  digitalWrite(trigPin2, HIGH);
   delayMicroseconds(10);
   digitalWrite(trigPin, LOW);
-  // Reads the echoPin, returns the sound wave travel time in microseconds
+  digitalWrite(trigPin2, LOW);
   duration = pulseIn(echoPin, HIGH);
-  // Calculating the distance
-  distance = duration * 0.034 / 2; // Speed of sound wave divided by 2 (go and back)
-  // Displays the distance on the Serial Monitor
+  duration2 = pulseIn(echoPin2, HIGH);
+  distance = duration * 0.034 / 2; 
+  distance2 = duration2 *0.034 /2;
   Serial.print("Distance: ");
   Serial.print(distance);
   Serial.println(" cm");
+  Serial.print(" Distance2: ");
+  Serial.print(distance2);
+  Serial.println(" cm");
   
-  if(distance<30)
+  
+  if(distance2<10){
+    digitalWrite(LED2, HIGH);   
+  	delay(100);                      
+  	digitalWrite(LED2, LOW);
+    tone(buzzer, 1000); // Send 1KHz sound signal...
+  	delay(1000);        // ...for 1 sec
+  	noTone(buzzer);     // Stop sound...
+  	delay(1000);
+    Serial.println("The dustbin is full. Please Empty it to Use Again.");
+    servoMotor.write(0);
+    digitalWrite(LED, LOW);
+   }
+   else{
+     digitalWrite(LED2, LOW);  
+     
+     if(distance<30)
   {
-    servoMotor.write(180);  // Turn Servo back to center position (90 degrees)
-    digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
-  	delay(500);                       // wait for a second
-  	digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
+    servoMotor.write(180);  
+    digitalWrite(LED, HIGH);   
+  	delay(500);                       
+  	digitalWrite(LED, LOW);   
     delay(3000);
     
   }
    else{
      servoMotor.write(0);
-     digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
+     digitalWrite(LED, LOW);    
+     delay(50);
+   };
      delay(50);
    }
 }
